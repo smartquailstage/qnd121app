4f7271a6416707ae8b07140bdcad35c6f1a36a91
smartquailstage <smartquail.stage@gmail.com>
2025-04-07 20:03:04 -0500

    Update urls.py

--- qnd121app/qnd121app/urls.py
+++ qnd121app/qnd121app/urls.py
@@ -69,10 +69,7 @@ urlpatterns = [
 
    
 
-  ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+  ]  
 
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+  # Add static URLs to the urlpatterns if in debug mode
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
